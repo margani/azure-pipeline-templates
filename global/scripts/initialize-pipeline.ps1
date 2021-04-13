f15c0f57756fcf06799d3d8ba84b67feb96faecd
Hossein Margani <margani.h@gmail.com>
2021-04-13 10:29:27 +0100

    Add environments and initialize template

---
+++ global/scripts/initialize-pipeline.ps1
@@ -0,0 +1,22 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()]
+    [String]
+    $EnvSubscriptions
+)
+
+Function Initialize-Pipeline {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [String]
+        $EnvSubscriptions
+    )
+    Process {
+        Write-Host "EnvSubscriptions: $EnvSubscriptions"
+        Write-Output "##vso[task.setvariable variable=ci-subscription]myorg-devtest-subscription"
+    }
+}
+
+Initialize-Pipeline `
+    -EnvSubscriptions $EnvSubscriptions
