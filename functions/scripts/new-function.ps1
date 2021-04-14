4bd5794c9dffc9feca1fe5456237603e8ff7c804
Hossein Margani <margani.h@gmail.com>
2021-04-14 09:04:29 +0100

    Add env subscription variable and a new template to test it

---
+++ functions/scripts/new-function.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()][String]$EnvSubscriptions,
+    [Parameter()][String]$EnvSubscription
+)
+
+Function New-Function {
+    [CmdletBinding()]
+    param (
+        [Parameter()][String]$EnvSubscriptions,
+        [Parameter()][String]$EnvSubscription
+    )
+    Process {
+        Write-Host "EnvSubscriptions: $EnvSubscriptions"
+        Write-Host "EnvSubscription: $EnvSubscription"
+    }
+}
+
+New-Function `
+    -EnvSubscriptions $EnvSubscriptions `
+    -EnvSubscription $EnvSubscription
--- global/scripts/initialize-pipeline.ps1
+++ global/scripts/initialize-pipeline.ps1
@@ -1,20 +1,31 @@
 ﻿[CmdletBinding()]
 param (
-    [Parameter()]
-    [String]
-    $EnvSubscriptions
+    [Parameter()][String]$EnvSubscriptions
 )
 
 Function Initialize-Pipeline {
     [CmdletBinding()]
     param (
-        [Parameter()]
-        [String]
-        $EnvSubscriptions
+        [Parameter()][String]$EnvSubscriptions
     )
     Process {
         Write-Host "EnvSubscriptions: $EnvSubscriptions"
-        Write-Output "##vso[task.setvariable variable=ci-subscription]myorg-devtest-subscription"
+
+        $EnvSubscriptions.Split(';', [System.StringSplitOptions]::RemoveEmptyEntries)
+        | ForEach-Object {
+            $envSubscription = $_
+            $keyValue = $envSubscription.Split(':', [System.StringSplitOptions]::RemoveEmptyEntries)
+            if ($keyValue.Length -eq 2) {
+                $envs = $keyValue[0]
+                $subscription = $keyValue[1].Trim()
+
+                $envs.Split(',', [System.StringSplitOptions]::RemoveEmptyEntries)
+                | ForEach-Object {
+                    $env = $_.Trim()
+                    Write-Output "##vso[task.setvariable variable=$env-subscription]$subscription"
+                }
+            }
+        }
     }
 }
 
