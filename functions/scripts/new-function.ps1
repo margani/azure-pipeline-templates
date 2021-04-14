aac672847aff5dc5d9ace07cf157eb6fea0d5cf0
Hossein Margani <margani.h@gmail.com>
2021-04-14 09:18:49 +0100

    Add product name variable

--- functions/scripts/new-function.ps1
+++ functions/scripts/new-function.ps1
@@ -1,21 +1,25 @@
 ﻿[CmdletBinding()]
 param (
     [Parameter()][String]$EnvSubscriptions,
-    [Parameter()][String]$EnvSubscription
+    [Parameter()][String]$EnvSubscription,
+    [Parameter()][String]$Product
 )
 
 Function New-Function {
     [CmdletBinding()]
     param (
         [Parameter()][String]$EnvSubscriptions,
-        [Parameter()][String]$EnvSubscription
+        [Parameter()][String]$EnvSubscription,
+        [Parameter()][String]$Product
     )
     Process {
         Write-Host "EnvSubscriptions: $EnvSubscriptions"
         Write-Host "EnvSubscription: $EnvSubscription"
+        Write-Host "Product: $Product"
     }
 }
 
 New-Function `
     -EnvSubscriptions $EnvSubscriptions `
-    -EnvSubscription $EnvSubscription
+    -EnvSubscription $EnvSubscription `
+    -Product $Product
