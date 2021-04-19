24a2c143edfe6ece8d7481679d0b22bd8487da1e
Hossein Margani <margani.h@gmail.com>
2021-04-19 09:33:34 +0100

    Add template for testign variables

---
+++ tests/scripts/test-variables.ps1
@@ -0,0 +1,27 @@
+﻿[CmdletBinding()]
+param (
+    [Parameter()][String]$Name,
+    [Parameter()][String]$Expected,
+    [Parameter()][String]$Actual
+)
+
+Function Test-Variables {
+    [CmdletBinding()]
+    param (
+        [Parameter()][String]$Name,
+        [Parameter()][String]$Expected,
+        [Parameter()][String]$Actual
+    )
+    Process {
+        if ($Expected -ne $Actual) {
+            throw "Value for $Name was expected to be $Expected but it's $Actual"
+        } else {
+            Write-Host "Value for $Name is as expected ($Expected): $Actual"
+        }
+    }
+}
+
+Test-Variables `
+    -Name $Name `
+    -Expected $Expected `
+    -Actual $Actual
