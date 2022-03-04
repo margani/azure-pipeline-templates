ca656c1540349c63611438f4f88df615a3a67ee0
Hossein Margani <hossein@margani.dev>
2022-03-04 09:03:39 +0000

    Add Get-Pipeline-Values helper function

--- functions/scripts/new-function.ps1
+++ functions/scripts/new-function.ps1
@@ -5,6 +5,9 @@ param (
     [Parameter()][String]$Product
 )
 
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Path
+Import-Module "$scriptPath/../../pipeline/scripts/helpers.psm1"
+
 Function New-Function {
     [CmdletBinding()]
     param (
@@ -16,6 +19,21 @@ Function New-Function {
         Write-Host "EnvSubscriptions: $EnvSubscriptions"
         Write-Host "EnvSubscription: $EnvSubscription"
         Write-Host "Product: $Product"
+
+        $pipelineValues = Get-PipelineValues
+        Write-Host "EnvSubscriptions: $($pipelineValues.EnvSubscriptions)"
+        Write-Host "EnvSubscription: $($pipelineValues.EnvSubscription)"
+        Write-Host "A1EnvSubscription: $($pipelineValues.A1EnvSubscription)"
+        Write-Host "ProductName: $($pipelineValues.ProductName)"
+
+        Write-Host "EnvSubscriptions2: $($pipelineValues.EnvSubscriptions2)"
+        Write-Host "A1EnvSubscription2: $($pipelineValues.A1EnvSubscription2)"
+        Write-Host "ProductName2: $($pipelineValues.ProductName2)"
+
+        Write-Host "EnvSubscriptions3: $($pipelineValues.EnvSubscriptions3)"
+        Write-Host "EnvSubscription3: $($pipelineValues.EnvSubscription3)"
+        Write-Host "A1EnvSubscription3: $($pipelineValues.A1EnvSubscription3)"
+        Write-Host "ProductName3: $($pipelineValues.ProductName3)"
     }
 }
 
--- pipeline/scripts/helpers.psm1
+++ pipeline/scripts/helpers.psm1
@@ -1,6 +1,34 @@
 ﻿Function Get-EnvSubscriptionVarName($Env) {
     if ($Env) {
-        return "env-$Env-subscription"
+        return "env($Env)subscription"
     }
-    return "env-subscription"
+    return "envsubscription"
+}
+
+Function Get-EnvVar($VariableName) {
+    return Get-ChildItem "Env:$($VariableName)*" | Where-Object { $_.Name -eq $VariableName } | Select-Object -ExpandProperty Value
+}
+
+Function Get-PipelineValues() {
+    $env = [System.Environment]::GetEnvironmentVariable("env", 'Process')
+    $envSubscriptionVarName = Get-EnvSubscriptionVarName($env)
+    $envA1SubscriptionVarName = Get-EnvSubscriptionVarName("a1")
+
+    $pipelineValues =  @{
+        EnvSubscriptions = [System.Environment]::GetEnvironmentVariable("varSubscriptions", 'Process')
+        EnvSubscription = [System.Environment]::GetEnvironmentVariable($envSubscriptionVarName, 'Process')
+        A1EnvSubscription = [System.Environment]::GetEnvironmentVariable($envA1SubscriptionVarName, 'Process')
+        ProductName = [System.Environment]::GetEnvironmentVariable("productName", 'Process')
+
+        EnvSubscriptions2 = $Env:varSubscriptions
+        A1EnvSubscription2 = $env:enva1subscription
+        ProductName2 = $Env:productName
+
+        EnvSubscriptions3 = Get-EnvVar("varSubscriptions")
+        EnvSubscription3 = Get-EnvVar($envSubscriptionVarName)
+        A1EnvSubscription3 = Get-EnvVar($envA1SubscriptionVarName)
+        ProductName3 = Get-EnvVar("productName")
+    }
+
+    return $pipelineValues
 }
